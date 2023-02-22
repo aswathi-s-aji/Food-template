ad6e9c5d2f9c3d2d94ab73a8f2d261650a99b438
Aswathi <aswathyaji2018@gmail.com>
2023-02-22 18:01:07 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function validate() {
+	let name = document.getElementById("a").value;
+	let nregx = /^[a-z A-Z]{2,15}$/;
+	let mail = document.getElementById("b").value;
+	let eregx = /^[a-zA-Z0-9+_.-]+@[a-zA-Z]+\.[a-z A-Z]{2,4}$/;
+	if (name == "") {
+		alert("Name field is mandatory");
+		return false;
+	} else if (!nregx.test(name)) {
+		alert("Enter a valid name");
+		return false;
+	} else if (mail == "") {
+		alert("Email field is mandatory");
+		return false;
+	} else if (!eregx.test(mail)) {
+		alert("Enter a valid mail id");
+		return false;
+	} else {
+		return true;
+	}
+}
